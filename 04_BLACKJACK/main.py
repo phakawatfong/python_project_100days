32f0c467f44f29cbaca87d45b2be0eb3c4917d60
phakawatfong <105853659+phakawatfong@users.noreply.github.com>
2022-08-23 18:04:48 +0700

    add blackjack.py

---
+++ 04_BLACKJACK/main.py
@@ -0,0 +1,107 @@
+############### Blackjack Project #####################
+
+#Difficulty Normal 😎: Use all Hints below to complete the project.
+#Difficulty Hard 🤔: Use only Hints 1, 2, 3 to complete the project.
+#Difficulty Extra Hard 😭: Only use Hints 1 & 2 to complete the project.
+#Difficulty Expert 🤯: Only use Hint 1 to complete the project.
+
+############### Our Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
+
+#11 is the Ace.
+#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+from art import logo
+
+# deal function
+def deal_card():
+  """Returns a random card from the deck."""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+# score calculation function
+def calculate_score(cards):
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+
+  if 11 in cards and sum(cards) > 21:
+    cards.remove(11)
+    cards.append(1)
+  return sum(cards)
+
+def compare_score(user_score, computer_score):
+  if user_score > 21 and computer_score >21:
+    print("")
+    return "You went over. You lose 😤"
+
+  if user_score == computer_score:
+    print("")
+    return "Draw\n"
+  elif user_score == 0:
+    print("")
+    return "You win with BLACKJACK\n"
+  elif computer_score == 0:
+    print("")
+    return "Computer win with BLACKJACK\n"
+  elif user_score > 21:
+    print("")
+    return "Lose, You went over 21\n"
+  elif computer_score > 21:
+    print("")
+    return "Win, computer went over 21\n"
+  elif user_score > computer_score:
+    print("")
+    return "You win\n"
+
+
+# PLAY GAME
+def play_game():
+        
+  user_cards = []
+  computer_cards = []
+  is_game_over = False
+  
+  for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+  
+  while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"   Your cards: {user_cards}, current score: {user_score}")
+    print(f"   Computer's first card: {computer_cards[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+      is_game_over = True
+    else:
+      draw = input("Do you want to draw another card 'yes' or 'no' ? ")
+      if draw[0].lower() == 'y':
+        user_cards.append(deal_card())
+      else:
+        is_game_over = True
+
+  while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
+
+  print(f"   Your final hand: {user_cards}, final score: {user_score}")
+  print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
+  print(compare_score(user_score, computer_score))
+
+print(logo)
+
+play_game()
+  
+
